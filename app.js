1251771f843fa5804d1c8cbefa5499e37ea424d0
bndao <bndao@student.42.fr>
2016-06-18 05:51:56 +0200

    jumpstartApp

---
+++ app.js
@@ -0,0 +1,39 @@
+var app = angular.module('LinksNews', ['ngRoute'])
+
+app.config(function($routeProvider){
+  $routeProvider.when('/', {
+    controller: 'PageCtrl',
+    templateUrl: 'linksPosted.html'
+  })
+  .otherwise('/');
+})
+
+app.factory('postedLinks', [function(){
+	var o = {
+		posted: [],
+		incUpVotes: function(link){
+			link.upvotes += 1;
+		}
+	};
+	return o;
+}]);
+
+app.controller('PageCtrl', ['$scope', 'postedLinks',
+	function($scope, postedLinks) {
+		$scope.linksData = postedLinks.posted;
+		$scope.incrementUpvotes = postedLinks.incUpVotes;
+		$scope.addLink = function(){
+			if(!$scope.title || $scope.title === '') { return; }
+			$scope.linksData.push({
+				title: $scope.title,
+				link: $scope.link,
+				upvotes: 0
+			});
+		$scope.title = '';
+		$scope.link = '';
+		};
+}]);
+
+app.run(function ($templateCache){
+ $templateCache.put('linksPosted.html');
+});
