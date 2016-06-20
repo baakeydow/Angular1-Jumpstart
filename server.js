007c5a578929aaa8130a62e1fb431bf3df6df180
bndao <bndao@student.42.fr>
2016-06-20 03:14:59 +0200

    http-auth for the express framework integration

--- server.js
+++ server.js
@@ -3,8 +3,18 @@ var path = require('path');
 var http = require('http');
 var fs = require('fs');
 
-var app = express();
+// Authentication module.
+var auth = require('http-auth');
+var basic = auth.basic({
+	realm: "Nope It's Private",
+	file: __dirname + "/users.htpasswd"
+});
 
-app.use(express.static(path.join(__dirname, '/public')));
+// Application setup.
+var app = express();
+app.use(auth.connect(basic));
+app.use(express.static(path.join(__dirname, 'public')));
+app.disable('x-powered-by');
+
 // Create an HTTP service.
 http.createServer(app).listen(8000);
